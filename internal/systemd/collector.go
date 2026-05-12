c5f87cc7b1c16a9f6e1214e23ddb3d297d605b76
unemployedCloud <playfulCloud@proton.me>
2026-05-12 22:59:30 +0200

    feat(parser): Add output command parses and its tests

--- internal/systemd/collector.go
+++ internal/systemd/collector.go
@@ -1,1 +1,7 @@
 package systemd
+
+import "github.com/playfulCloud/unitop/internal/store"
+
+type Collector struct {
+	Store store.ServiceStore
+}
--- internal/systemd/parser.go
+++ internal/systemd/parser.go
@@ -1,1 +1,16 @@
 package systemd
+
+import "strings"
+
+func parseCommandOutput(commandOutput string) map[string]string {
+	lines := strings.SplitSeq(commandOutput, "\n")
+	properties := make(map[string]string)
+
+	for line := range lines {
+		keyValueParts := strings.SplitN(line, "=", 2)
+		if len(keyValueParts) == 2 {
+			properties[keyValueParts[0]] = keyValueParts[1]
+		}
+	}
+	return properties
+}
